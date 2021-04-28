33cbd320f29cdbda64b888a4b1756f9eb60bab00
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-04-28 20:02:45 +0530

    some changes in root files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import "./App.css";
+import Editor from "./components/Editor/Editor";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { AppContext } from "./contexts/AppContext";
+import EditorContextProvider from "./contexts/EditorContext";
+import SidebarContextProvider from "./contexts/SidebarContext";
+import { invokeFirestore } from "./firebase";
 
 function App() {
-  return <div className="App">Hello World!!</div>;
+  const { selectedNoteIndex, selectedNote, notes, setNotes } = useContext(
+    AppContext
+  );
+
+  useEffect(() => {
+    invokeFirestore.collection("notes").onSnapshot((snapshot) => {
+      const getNotes = snapshot.docs.map((doc) => {
+        const data = doc.data();
+        data["id"] = doc.id;
+        return data;
+      });
+      setNotes(getNotes);
+      console.log(notes);
+    });
+  }, []);
+
+  return (
+    <div className="app-container">
+      <SidebarContextProvider>
+        <EditorContextProvider>
+          <Sidebar />
+          {selectedNote ? <Editor /> : null}
+        </EditorContextProvider>
+      </SidebarContextProvider>
+    </div>
+  );
 }
 
 export default App;
