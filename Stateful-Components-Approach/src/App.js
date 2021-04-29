9a3e3b86b55b8f6187ebc630037acb2bd1f8cd04
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-04-29 22:05:24 +0530

    App component

--- Stateful-Components-Approach/src/App.js
+++ Stateful-Components-Approach/src/App.js
@@ -1,10 +1,104 @@
 import React, { Component } from "react";
+import { invokeFirestore, timestamp } from "./firebase";
+import Editor from "./components/Editor/Editor";
+import Sidebar from "./components/Sidebar/Sidebar";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return <div>Hello World!!</div>;
+  constructor() {
+    super();
+
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null,
+    };
   }
+
+  componentDidMount = () => {
+    invokeFirestore.collection("notes").onSnapshot((snapshot) => {
+      const notes = snapshot.docs.map((doc) => {
+        const data = doc.data();
+        data["id"] = doc.id;
+        return data;
+      });
+      console.log(notes);
+      this.setState({ notes });
+    });
+  };
+
+  render() {
+    return (
+      <div className="app-container">
+        <Sidebar
+          selectedNoteIndex={this.state.selectedNoteIndex}
+          notes={this.state.notes}
+          deleteNote={this.deleteNote}
+          selectNote={this.selectNote}
+          newNote={this.newNote}
+        ></Sidebar>
+        {this.state.selectedNote ? (
+          <Editor
+            selectedNote={this.state.selectedNote}
+            selectedNoteIndex={this.state.selectedNoteIndex}
+            notes={this.state.notes}
+            noteUpdate={this.noteUpdate}
+          ></Editor>
+        ) : null}
+      </div>
+    );
+  }
+
+  selectNote = (note, index) =>
+    this.setState({ selectedNoteIndex: index, selectedNote: note });
+
+  noteUpdate = (id, noteObj) => {
+    invokeFirestore.collection("notes").doc(id).update({
+      title: noteObj.title,
+      body: noteObj.body,
+      timestamp: timestamp,
+    });
+  };
+
+  newNote = async (title) => {
+    const note = {
+      title: title,
+      body: "",
+    };
+    const newFromDB = await invokeFirestore.collection("notes").add({
+      title: note.title,
+      body: note.body,
+      timestamp: timestamp,
+    });
+    const newID = newFromDB.id;
+    await this.setState({ notes: [...this.state.notes, note] });
+    const newNoteIndex = this.state.notes.indexOf(
+      this.state.notes.filter((_note) => _note.id === newID)[0]
+    );
+    this.setState({
+      selectedNote: this.state.notes[newNoteIndex],
+      selectedNoteIndex: newNoteIndex,
+    });
+  };
+
+  deleteNote = async (note) => {
+    const noteIndex = this.state.notes.indexOf(note);
+    await this.setState({
+      notes: this.state.notes.filter((_note) => _note !== note),
+    });
+    if (this.state.selectedNoteIndex === noteIndex) {
+      this.setState({ selectedNoteIndex: null, selectedNote: null });
+    } else {
+      this.state.notes.length > 1
+        ? this.selectNote(
+            this.state.notes[this.state.selectedNoteIndex - 1],
+            this.state.selectedNoteIndex - 1
+          )
+        : this.setState({ selectedNoteIndex: null, selectedNote: null });
+    }
+
+    invokeFirestore.collection("notes").doc(note.id).delete();
+  };
 }
 
 export default App;
