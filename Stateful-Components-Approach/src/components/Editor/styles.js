aa5f158dbc662769e1c0bf7ae69db3a8bf76abeb
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-04-29 22:07:24 +0530

    Editor component

---
+++ Stateful-Components-Approach/src/components/Editor/styles.js
@@ -0,0 +1,36 @@
+const styles = (theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    height: "calc(100% - 35px)",
+    position: "absolute",
+    left: "0",
+    width: "300px",
+    boxShadow: "0px 0px 2px black",
+  },
+  titleInput: {
+    height: "50px",
+    boxSizing: "border-box",
+    border: "none",
+    padding: "5px",
+    fontSize: "24px",
+    width: "calc(100% - 300px)",
+    backgroundColor: "#29487d",
+    color: "white",
+    paddingLeft: "20px",
+  },
+  editIcon: {
+    position: "absolute",
+    left: "380px",
+    top: "45px",
+    color: "white",
+    width: "10",
+    height: "10",
+  },
+  editorContainer: {
+    height: "100%",
+    boxSizing: "border-box",
+    border: "5px solid white",
+  },
+});
+
+export default styles;
